719627dc77896041d39b27710cbfda90af323610
hibaabdulrahman <87442633+hibaabdulrahman@users.noreply.github.com>
2021-07-27 14:18:05 +0400

    Number of needed transmissions in D2D phase

---
+++ D2D6.m
@@ -0,0 +1,27 @@
+clc;clear all;close all
+
+M=1:5:30;
+% load BELR
+for jj=1:length(M)
+    
+    for kk=1:3
+  
+      BELR=kk.*rand(1,M(jj))./M(jj);
+Ei=BELR;
+Exlegacy=sum(1./(1-Ei));
+ExD(jj,kk)=M(jj)./(1-max(Ei));
+
+
+% G(jj,kk)=(Exlegacy-ExD)./Exlegacy;
+    end
+%    tim(jj)=mean(ExD);
+ 
+end
+figure
+semilogy(0:4:20,ExD(:,1))
+hold on
+semilogy(0:4:20,ExD(:,2),'r') 
+hold on
+semilogy(0:4:20,ExD(:,3),'g')
+
+
